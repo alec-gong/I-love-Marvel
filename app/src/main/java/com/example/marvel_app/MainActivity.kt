13fb957131052b5d209ef696ddc25171885a4743
alex-r-gong <64893409+alex-r-gong@users.noreply.github.com>
2023-06-08 16:29:53 -0400

    Successfully parsed json info

--- app/src/main/java/com/example/marvel_app/MainActivity.kt
+++ app/src/main/java/com/example/marvel_app/MainActivity.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.Response
+import org.json.JSONException
+import org.json.JSONObject
 import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
@@ -21,6 +23,20 @@ class MainActivity : AppCompatActivity() {
 
             override fun onResponse(call: Call, response: Response) {
                 Log.d("API success", response.toString())
+
+                // extract json information
+                val responseBody = response.body?.string()
+                if(response.isSuccessful && responseBody != null){
+                    try{
+                        // extract information
+                        val jsonResponse = JSONObject(responseBody)
+                        Log.d("API success", jsonResponse.toString())
+                    } catch(e: JSONException) {
+                        Log.e("API error", "Error parsing JSON: ${e.message}")
+                    }
+                } else {
+                    Log.d("API error", "Response unsuccessful")
+                }
             }
         })
     }
