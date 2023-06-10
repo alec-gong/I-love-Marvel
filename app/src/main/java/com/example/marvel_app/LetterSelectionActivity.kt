0d4c24ecde42c7fb1ac391e9a01347d92c265ef2
alex-r-gong <64893409+alex-r-gong@users.noreply.github.com>
2023-06-09 21:47:28 -0400

    Finish landing page

---
+++ app/src/main/java/com/example/marvel_app/LetterSelectionActivity.kt
@@ -0,0 +1,53 @@
+package com.example.marvel_app
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class LetterSelectionActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_letter_selection)
+
+        val letterButtons = listOf<Button>(
+            findViewById(R.id.btn_A),
+            findViewById(R.id.btn_B),
+            findViewById(R.id.btn_C),
+            findViewById(R.id.btn_D),
+            findViewById(R.id.btn_E),
+            findViewById(R.id.btn_F),
+            findViewById(R.id.btn_G),
+            findViewById(R.id.btn_G),
+            findViewById(R.id.btn_I),
+            findViewById(R.id.btn_J),
+            findViewById(R.id.btn_K),
+            findViewById(R.id.btn_L),
+            findViewById(R.id.btn_M),
+            findViewById(R.id.btn_N),
+            findViewById(R.id.btn_O),
+            findViewById(R.id.btn_P),
+            findViewById(R.id.btn_Q),
+            findViewById(R.id.btn_R),
+            findViewById(R.id.btn_S),
+            findViewById(R.id.btn_T),
+            findViewById(R.id.btn_U),
+            findViewById(R.id.btn_V),
+            findViewById(R.id.btn_W),
+            findViewById(R.id.btn_X),
+            findViewById(R.id.btn_Y),
+            findViewById(R.id.btn_Z)
+        )
+
+        // Set click listeners for each letter button
+        for (button in letterButtons) {
+            button.setOnClickListener {
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("letter", button.text.toString())
+                startActivity(intent)
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/example/marvel_app/MainActivity.kt
+++ app/src/main/java/com/example/marvel_app/MainActivity.kt
@@ -23,76 +23,80 @@ class MainActivity : AppCompatActivity() {
         characterList = mutableListOf()
         rvCharacters = findViewById(R.id.character_list)
 
+        val letter = intent.getStringExtra("letter")
+
         val marvelApiClient = MarvelApiClient()
-        marvelApiClient.fetchCharacters(object: Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                Log.d("API error", e.toString())
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                Log.d("API success", "success")
-
-                // extract json information
-                val responseBody = response.body?.string()
-
-                if(response.isSuccessful && responseBody != null){
-                    try{
-
-                        // extract information
-                        val jsonResponse = JSONObject(responseBody)
-                        val data = jsonResponse.getJSONObject("data")
-                        val results = data.getJSONArray("results")
-
-                        // extract all the characters
-                        for(i in 0 until results.length()){
-
-                            val character = results.getJSONObject(i)
-                            val name = character.getString("name")
-
-                            // get description; uses filler if none
-                            val description: String
-                            if(character.getString("description") == ""){
-                                description = "No description at the moment."
-                            } else {
-                                description = character.getString("description")
-                            }
-
-                            // filter out characters without images
-                            val thumbnail = character.getJSONObject("thumbnail")
-                            val thumbnailPath = thumbnail.getString("path")
-                            if(!thumbnailPath.endsWith("image_not_available")){
-                                val imageUrl = thumbnailPath + "." + thumbnail.getString("extension")
-                                val characterModel = CharacterModel(imageUrl, name, description)
-                                // add to character list
-                                characterList.add(characterModel)
-                            }
-                        }
-
-                        // initialize adapter
-                        runOnUiThread{
-                            val adapter = CharacterAdapter(characterList)
-                            rvCharacters.adapter = adapter
-                            rvCharacters.layoutManager = LinearLayoutManager(this@MainActivity)
-
-                            // Set item click listener on adapter
-                            adapter.setOnItemClickListener(object : CharacterAdapter.OnItemClickListener {
-                                override fun onItemClick(character: CharacterModel) {
-                                    val intent = Intent(this@MainActivity, CharacterDetailsActivity::class.java)
-                                    intent.putExtra("characterImage", character.characterImage)
-                                    intent.putExtra("characterName", character.characterName)
-                                    intent.putExtra("characterDescription", character.characterDescription)
-                                    startActivity(intent)
-                                }
-                            })
-                        }
-
-                    } catch(e: JSONException) {
-                        Log.e("API error", "Error parsing JSON: ${e.message}")
-                    }
-                } else {
-                    Log.d("API error", "Response unsuccessful")
+        if (letter != null) {
+            marvelApiClient.fetchCharacters(letter, object: Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    Log.d("API error", e.toString())
                 }
-            }
-        })
+
+                override fun onResponse(call: Call, response: Response) {
+                    Log.d("API success", "success")
+
+                    // extract json information
+                    val responseBody = response.body?.string()
+
+                    if(response.isSuccessful && responseBody != null){
+                        try{
+
+                            // extract information
+                            val jsonResponse = JSONObject(responseBody)
+                            val data = jsonResponse.getJSONObject("data")
+                            val results = data.getJSONArray("results")
+
+                            // extract all the characters
+                            for(i in 0 until results.length()){
+
+                                val character = results.getJSONObject(i)
+                                val name = character.getString("name")
+
+                                // get description; uses filler if none
+                                val description: String
+                                if(character.getString("description") == ""){
+                                    description = "No description at the moment."
+                                } else {
+                                    description = character.getString("description")
+                                }
+
+                                // filter out characters without images
+                                val thumbnail = character.getJSONObject("thumbnail")
+                                val thumbnailPath = thumbnail.getString("path")
+                                if(!thumbnailPath.endsWith("image_not_available")){
+                                    val imageUrl = thumbnailPath + "." + thumbnail.getString("extension")
+                                    val characterModel = CharacterModel(imageUrl, name, description)
+                                    // add to character list
+                                    characterList.add(characterModel)
+                                }
+                            }
+
+                            // initialize adapter
+                            runOnUiThread{
+                                val adapter = CharacterAdapter(characterList)
+                                rvCharacters.adapter = adapter
+                                rvCharacters.layoutManager = LinearLayoutManager(this@MainActivity)
+
+                                // Set item click listener on adapter
+                                adapter.setOnItemClickListener(object : CharacterAdapter.OnItemClickListener {
+                                    override fun onItemClick(character: CharacterModel) {
+                                        val intent = Intent(this@MainActivity, CharacterDetailsActivity::class.java)
+                                        intent.putExtra("characterImage", character.characterImage)
+                                        intent.putExtra("characterName", character.characterName)
+                                        intent.putExtra("characterDescription", character.characterDescription)
+                                        startActivity(intent)
+                                    }
+                                })
+                            }
+
+                        } catch(e: JSONException) {
+                            Log.e("API error", "Error parsing JSON: ${e.message}")
+                        }
+                    } else {
+                        Log.d("API error", "Response unsuccessful")
+                    }
+                }
+            })
+        }
     }
 }
